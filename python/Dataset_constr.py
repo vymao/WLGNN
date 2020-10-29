a19a86978ef3d085225c72db3569a2da2e71e993
Victor <victor.mao@berkeley.edu>
2020-10-29 17:13:44 -0400

    Added dataset construction methods

--- python/Dataset_constr.py
+++ python/Dataset_constr.py
@@ -22,7 +22,6 @@ from torch.utils.data import DataLoader
 from torch_sparse import coalesce
 from torch_scatter import scatter_min
 import torch_geometric.transforms as T
-from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import GCNConv, SAGEConv, global_sort_pool, global_add_pool
 from torch_geometric.data import Data, Dataset, InMemoryDataset, DataLoader
 from torch_geometric.utils import (negative_sampling, add_self_loops,
@@ -35,105 +34,84 @@ import warnings
 from scipy.sparse import SparseEfficiencyWarning
 warnings.simplefilter('ignore',SparseEfficiencyWarning)
 
-from utils import *
-
-class SEALDataset(InMemoryDataset):
-    def __init__(self, root, data, split_edge, num_hops, percent=100, split='train', 
-                 use_coalesce=False, node_label='drnl', ratio_per_hop=1.0, 
-                 max_nodes_per_hop=None):
-        self.data = data
-        self.split_edge = split_edge
-        self.num_hops = num_hops
-        self.percent = int(percent) if percent >= 1.0 else percent
-        self.split = split
-        self.use_coalesce = use_coalesce
-        self.node_label = node_label
-        self.ratio_per_hop = ratio_per_hop
-        self.max_nodes_per_hop = max_nodes_per_hop
-        super(SEALDataset, self).__init__(root)
-        self.data, self.slices = torch.load(self.processed_paths[0])
-
-    @property
-    def processed_file_names(self):
-        if self.percent == 100:
-            name = 'SEAL_{}_data'.format(self.split)
-        else:
-            name = 'SEAL_{}_data_{}'.format(self.split, self.percent)
-        name += '.pt'
-        return [name]
-
-    def process(self):
-        pos_edge, neg_edge = get_pos_neg_edges(self.split, self.split_edge, 
-                                               self.data.edge_index, 
-                                               self.data.num_nodes, 
-                                               self.percent)
-
-        if self.use_coalesce:  # compress mutli-edge into edge with weight
-            self.data.edge_index, self.data.edge_weight = coalesce(
-                self.data.edge_index, self.data.edge_weight, 
-                self.data.num_nodes, self.data.num_nodes)
-
-        if 'edge_weight' in self.data:
-            edge_weight = self.data.edge_weight.view(-1)
-        else:
-            edge_weight = torch.ones(self.data.edge_index.size(1), dtype=int)
-        A = ssp.csr_matrix(
-            (edge_weight, (self.data.edge_index[0], self.data.edge_index[1])), 
-            shape=(self.data.num_nodes, self.data.num_nodes)
-        )
-        
-        # Extract enclosing subgraphs for pos and neg edges
-        pos_list = extract_enclosing_subgraphs(
-            pos_edge, A, self.data.x, "pos", self.num_hops, 
-            self.node_label, self.ratio_per_hop, self.max_nodes_per_hop)
-        neg_list = extract_enclosing_subgraphs(
-            neg_edge, A, self.data.x, "neg", self.num_hops, 
-            self.node_label, self.ratio_per_hop, self.max_nodes_per_hop)
-
-        torch.save(self.collate(pos_list + neg_list), self.processed_paths[0])
-        del pos_list, neg_list
+import Dataset_constr, enclosing_subgraph
 
 
+def main(): 
+	args = parse_args()
+	dataset = PygLinkPropPredDataset(name=args.dataset)
 
-def get_pos_neg_edges(split, split_edge, edge_index, num_nodes, percent=100):
-    if 'edge' in split_edge['train']:
-        pos_edge = split_edge[split]['edge'].t()
-        if split == 'train':
-            new_edge_index, _ = add_self_loops(edge_index)
-            neg_edge = negative_sampling(
-                new_edge_index, num_nodes=num_nodes,
-                num_neg_samples=pos_edge.size(1))
-        else:
-            neg_edge = split_edge[split]['edge_neg'].t()
-        # subsample for pos_edge
-        np.random.seed(123)
-        num_pos = pos_edge.size(1)
-        perm = np.random.permutation(num_pos)
-        perm = perm[:int(percent / 100 * num_pos)]
-        pos_edge = pos_edge[:, perm]
-        # subsample for neg_edge
-        np.random.seed(123)
-        num_neg = neg_edge.size(1)
-        perm = np.random.permutation(num_neg)
-        perm = perm[:int(percent / 100 * num_neg)]
-        neg_edge = neg_edge[:, perm]
+    split_edge = dataset.get_edge_split()
 
-    elif 'source_node' in split_edge['train']:
-        source = split_edge[split]['source_node']
-        target = split_edge[split]['target_node']
-        if split == 'train':
-            target_neg = torch.randint(0, num_nodes, [target.size(0), 1],
-                                       dtype=torch.long)
-        else:
-            target_neg = split_edge[split]['target_node_neg']
-        # subsample
-        np.random.seed(123)
-        num_source = source.size(0)
-        perm = np.random.permutation(num_source)
-        perm = perm[:int(percent / 100 * num_source)]
-        source, target, target_neg = source[perm], target[perm], target_neg[perm, :]
-        pos_edge = torch.stack([source, target])
-        neg_per_target = target_neg.size(1)
-        neg_edge = torch.stack([source.repeat_interleave(neg_per_target), 
-                                target_neg.view(-1)])
-    return pos_edge, neg_edge
+	if args.save_appendix == '':
+	    args.save_appendix = '_' + time.strftime("%Y%m%d%H%M%S")
+	if args.data_appendix == '':
+	    args.data_appendix = '_h{}_rph{}'.format(
+	        args.num_hops, ''.join(str(args.ratio_per_hop).split('.')))
+	    if args.max_nodes_per_hop is not None:
+	        args.data_appendix += '_mnph{}'.format(args.max_nodes_per_hop)
+
+
+	path = dataset.root + '_wl{}'.format(args.data_appendix)
+	use_coalesce = False
+
+	train_dataset = eval("WLDataset")(
+	    path, 
+	    data, 
+	    split_edge, 
+	    num_hops=args.num_hops, 
+	    percent=args.train_percent, 
+	    split='train', 
+	    use_coalesce=use_coalesce, 
+	    node_label=args.node_label, 
+	    ratio_per_hop=args.ratio_per_hop, 
+	    max_nodes_per_hop=args.max_nodes_per_hop, 
+	) 
+
+	val_dataset = eval("WLDataset")(
+	    path, 
+	    data, 
+	    split_edge, 
+	    num_hops=args.num_hops, 
+	    percent=args.val_percent, 
+	    split='valid', 
+	    use_coalesce=use_coalesce, 
+	    node_label=args.node_label, 
+	    ratio_per_hop=args.ratio_per_hop, 
+	    max_nodes_per_hop=args.max_nodes_per_hop, 
+	)
+
+	test_dataset = eval("WLDataset")(
+	    path, 
+	    data, 
+	    split_edge, 
+	    num_hops=args.num_hops, 
+	    percent=args.test_percent, 
+	    split='test', 
+	    use_coalesce=use_coalesce, 
+	    node_label=args.node_label, 
+	    ratio_per_hop=args.ratio_per_hop, 
+	    max_nodes_per_hop=args.max_nodes_per_hop, 
+	)
+
+def parse_args();
+	parser = argparse.ArgumentParser(description='Dataset Creation')
+	parser.add_argument('--dataset', type=str, default='ogbl-collab')
+	parser.add_argument('--num_hops', type=int, default=1)
+	parser.add_argument('--ratio_per_hop', type=float, default=1.0)
+	parser.add_argument('--max_nodes_per_hop', type=int, default=None)
+	parser.add_argument('--train_percent', type=float, default=100)
+	parser.add_argument('--val_percent', type=float, default=100)
+	parser.add_argument('--test_percent', type=float, default=100)
+	parser.add_argument('--node_label', type=str, default='drnl', 
+                    help="which specific labeling trick to use")
+	parser.add_argument('--data_appendix', type=str, default='', 
+	                    help="an appendix to the data directory")
+	parser.add_argument('--save_appendix', type=str, default='', 
+	                    help="an appendix to the save directory")
+
+	return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/WLDataset.py
@@ -0,0 +1,117 @@
+import argparse
+import math
+import time
+import random
+import os, sys
+import os.path as osp
+from itertools import chain
+from shutil import copy
+import copy as cp
+from tqdm import tqdm
+import pdb
+
+import numpy as np
+from sklearn.metrics import roc_auc_score
+import scipy.sparse as ssp
+import torch
+import torch.nn.functional as F
+from torch.nn import BCEWithLogitsLoss
+from torch.nn import ModuleList, Linear, Conv1d, MaxPool1d, Embedding
+from torch.utils.data import DataLoader
+
+from torch_sparse import coalesce
+from torch_scatter import scatter_min
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv, SAGEConv, global_sort_pool, global_add_pool
+from torch_geometric.data import Data, Dataset, InMemoryDataset, DataLoader
+from torch_geometric.utils import (negative_sampling, add_self_loops,
+                                   train_test_split_edges, to_networkx, 
+                                   to_scipy_sparse_matrix, to_undirected)
+
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
+
+import warnings
+from scipy.sparse import SparseEfficiencyWarning
+warnings.simplefilter('ignore',SparseEfficiencyWarning)
+
+class WLDataset(InMemoryDataset):
+    def __init__(self, root, data, split_edge, num_hops, percent=100, split='train', 
+                 use_coalesce=False, node_label='drnl', ratio_per_hop=1.0, 
+                 max_nodes_per_hop=None):
+        self.data = data
+        self.split_edge = split_edge
+        self.num_hops = num_hops
+        self.percent = int(percent) if percent >= 1.0 else percent
+        self.split = split
+        self.use_coalesce = use_coalesce
+        self.node_label = node_label
+        self.ratio_per_hop = ratio_per_hop
+        self.max_nodes_per_hop = max_nodes_per_hop
+        super(SEALDataset, self).__init__(root)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def processed_file_names(self):
+        if self.percent == 100:
+            name = 'SEAL_{}_data'.format(self.split)
+        else:
+            name = 'SEAL_{}_data_{}'.format(self.split, self.percent)
+        name += '.pt'
+        return [name]
+
+    def process(self):
+        pos_edge, neg_edge = get_pos_neg_edges(self.split, self.split_edge, 
+                                               self.data.edge_index, 
+                                               self.data.num_nodes, 
+                                               self.percent)
+
+        if self.use_coalesce:  # compress mutli-edge into edge with weight
+            self.data.edge_index, self.data.edge_weight = coalesce(
+                self.data.edge_index, self.data.edge_weight, 
+                self.data.num_nodes, self.data.num_nodes)
+
+        if 'edge_weight' in self.data:
+            edge_weight = self.data.edge_weight.view(-1)
+        else:
+            edge_weight = torch.ones(self.data.edge_index.size(1), dtype=int)
+        A = ssp.csr_matrix(
+            (edge_weight, (self.data.edge_index[0], self.data.edge_index[1])), 
+            shape=(self.data.num_nodes, self.data.num_nodes)
+        )
+        
+        # Extract enclosing subgraphs for pos and neg edges
+        pos_list = extract_enclosing_subgraphs(
+            pos_edge, A, self.data.x, "pos", self.num_hops, 
+            self.node_label, self.ratio_per_hop, self.max_nodes_per_hop)
+        neg_list = extract_enclosing_subgraphs(
+            neg_edge, A, self.data.x, "neg", self.num_hops, 
+            self.node_label, self.ratio_per_hop, self.max_nodes_per_hop)
+
+        torch.save(self.collate(pos_list + neg_list), self.processed_paths[0])
+        del pos_list, neg_list
+
+
+
+def get_pos_neg_edges(split, split_edge, edge_index, num_nodes, percent=100):
+    pos_edge = split_edge[split]['edge'].t()
+    if split == 'train':
+        new_edge_index, _ = add_self_loops(edge_index)
+        neg_edge = negative_sampling(
+            new_edge_index, num_nodes=num_nodes,
+            num_neg_samples=pos_edge.size(1))
+    else:
+        neg_edge = split_edge[split]['edge_neg'].t()
+    # subsample for pos_edge
+    np.random.seed(123)
+    num_pos = pos_edge.size(1)
+    perm = np.random.permutation(num_pos)
+    perm = perm[:int(percent / 100 * num_pos)]
+    pos_edge = pos_edge[:, perm]
+    # subsample for neg_edge
+    np.random.seed(123)
+    num_neg = neg_edge.size(1)
+    perm = np.random.permutation(num_neg)
+    perm = perm[:int(percent / 100 * num_neg)]
+    neg_edge = neg_edge[:, perm]
+
+    return pos_edge, neg_edge
