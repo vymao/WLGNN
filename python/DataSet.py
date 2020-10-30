0de4cb32098eaaa3e421dbdcb69d1fe8caa327e4
Victor <victor.mao@berkeley.edu>
2020-10-30 18:07:11 -0400

    Fixed file names

--- python/DataSet.py
+++ python/DataSet.py
@@ -34,8 +34,6 @@ import warnings
 from scipy.sparse import SparseEfficiencyWarning
 warnings.simplefilter('ignore',SparseEfficiencyWarning)
 
-from enclosing_subgraph import *
-
 class WLDataset(InMemoryDataset):
     def __init__(self, root, data, split_edge, num_hops, percent=100, split='train', 
                  use_coalesce=False, node_label='drnl', ratio_per_hop=1.0, 
@@ -49,15 +47,15 @@ class WLDataset(InMemoryDataset):
         self.node_label = node_label
         self.ratio_per_hop = ratio_per_hop
         self.max_nodes_per_hop = max_nodes_per_hop
-        super(WLDataset, self).__init__(root)
+        super(SEALDataset, self).__init__(root)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def processed_file_names(self):
         if self.percent == 100:
-            name = 'WLSEAL_{}_data'.format(self.split)
+            name = 'SEAL_{}_data'.format(self.split)
         else:
-            name = 'WLSEAL_{}_data_{}'.format(self.split, self.percent)
+            name = 'SEAL_{}_data_{}'.format(self.split, self.percent)
         name += '.pt'
         return [name]
 
@@ -91,6 +89,59 @@ class WLDataset(InMemoryDataset):
 
         torch.save(self.collate(pos_list + neg_list), self.processed_paths[0])
         del pos_list, neg_list
+
+
+class WLDynamicDataset(Dataset):
+    def __init__(self, root, data, split_edge, num_hops, percent=100, split='train', 
+                 use_coalesce=False, node_label='drnl', ratio_per_hop=1.0, 
+                 max_nodes_per_hop=None, **kwargs):
+        self.data = data
+        self.split_edge = split_edge
+        self.num_hops = num_hops
+        self.percent = percent
+        self.use_coalesce = use_coalesce
+        self.node_label = node_label
+        self.ratio_per_hop = ratio_per_hop
+        self.max_nodes_per_hop = max_nodes_per_hop
+        super(SEALDynamicDataset, self).__init__(root)
+
+        pos_edge, neg_edge = get_pos_neg_edges(split, self.split_edge, 
+                                               self.data.edge_index, 
+                                               self.data.num_nodes, 
+                                               self.percent)
+        self.links = torch.cat([pos_edge, neg_edge], 1).t().tolist()
+        
+
+        self.labels = [1] * pos_edge.size(1) + [0] * neg_edge.size(1)
+
+        if self.use_coalesce:  # compress mutli-edge into edge with weight
+            self.data.edge_index, self.data.edge_weight = coalesce(
+                self.data.edge_index, self.data.edge_weight, 
+                self.data.num_nodes, self.data.num_nodes)
+
+        if 'edge_weight' in self.data:
+            edge_weight = self.data.edge_weight.view(-1)
+        else:
+            edge_weight = torch.ones(self.data.edge_index.size(1), dtype=int)
+        self.A = ssp.csr_matrix(
+            (edge_weight, (self.data.edge_index[0], self.data.edge_index[1])), 
+            shape=(self.data.num_nodes, self.data.num_nodes)
+        )
+        
+    def __len__(self):
+        return len(self.links)
+
+    def get(self, idx):
+        src, dst = self.links[idx]
+
+        if self.labels[idx]: status = "pos"
+        else: status = "neg"
+
+        tmp = k_hop_subgraph(src, dst, self.num_hops, self.A, status, self.ratio_per_hop, 
+                             self.max_nodes_per_hop, node_features=self.data.x)
+        data = construct_pyg_graph(*tmp, self.node_label)
+
+        return data
 
 
 
